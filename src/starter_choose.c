7b85b3f4d0bcafcfb3a9a55efa4437e3ce339cb4
The TwilightBlood <hwengerstickel@protonmail.com>
2022-09-04 21:23:12 -0500

    Swap around starters

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -117,9 +117,9 @@ static const u8 sStarterLabelCoords[STARTER_MON_COUNT][2] =
 
 static const u16 sStarterMon[STARTER_MON_COUNT] =
 {
-    SPECIES_CHIKORITA,
+    SPECIES_BULBASAUR,
     SPECIES_TORCHIC,
-    SPECIES_SQUIRTLE,
+    SPECIES_TOTODILE,
 };
 
 static const struct BgTemplate sBgTemplates[3] =
